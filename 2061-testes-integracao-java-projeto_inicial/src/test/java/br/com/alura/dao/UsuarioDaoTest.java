271e731c86362d4be930acd2b987b30c080b20c3
lysbaldrez <lyscalefi@gmail.com>
2021-03-14 20:17:12 -0400

    Melhorando o código e testando a classe LeilaoDao

--- 2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/dao/UsuarioDaoTest.java
+++ 2061-testes-integracao-java-projeto_inicial/src/test/java/br/com/alura/dao/UsuarioDaoTest.java
@@ -4,6 +4,8 @@ import br.com.alura.leilao.dao.UsuarioDao;
 import br.com.alura.leilao.model.Usuario;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import util.JPAUtil;
 
 import javax.persistence.EntityManager;
@@ -12,31 +14,44 @@ import javax.persistence.NoResultException;
 public class UsuarioDaoTest {
 
     private UsuarioDao dao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new UsuarioDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach () {
+        em.getTransaction().rollback();
+    }
 
     @Test
-    public void deveriaEncontrarUsuarioCadastrado () {
-        EntityManager em = JPAUtil.getEntityManager();
-        this.dao = new UsuarioDao(em);
-
-        Usuario usuario = new Usuario("fulano", "fulano@gmail.com", "12345678");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
-
+    void deveriaEncontrarUsuarioCadastrado () {
+        Usuario usuario = criarUsuario();
         Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
         Assert.assertNotNull(encontrado);
     }
 
     @Test
-    public void naoDeveriaEncontrarUsuarioNaoCadastrado () {
-        EntityManager em = JPAUtil.getEntityManager();
-        this.dao = new UsuarioDao(em);
-
-        Usuario usuario = new Usuario("fulano", "fulano@gmail.com", "12345678");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
-
+    void naoDeveriaEncontrarUsuarioNaoCadastrado () {
+        criarUsuario();
         Assert.assertThrows(NoResultException.class,() -> this.dao.buscarPorUsername("beltrano"));
+    }
+
+    @Test
+    void deveriaRemoverUmUsuario () {
+        Usuario usuario = criarUsuario();
+        dao.deletar(usuario);
+
+        Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername(usuario.getNome()));
+    }
+
+    private Usuario criarUsuario () {
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+        em.persist(usuario);
+        return usuario;
     }
 }
